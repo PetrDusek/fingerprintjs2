bbcf6e81e27f7e544666b7697d05487d63c6baa7
PetrDusek <petrdusek9@gmail.com>
2017-05-04 15:19:29 +0200

    add new getAdblock method

--- fingerprint2.js
+++ fingerprint2.js
@@ -768,14 +768,19 @@
     },
     getAdBlock: function(){
       var ads = document.createElement("div");
-      ads.setAttribute("id", "ads");
-      try {
-        // body may not exist, that's why we need try/catch
-        document.body.appendChild(ads);
-        return document.getElementById("ads") ? false : true;
-      } catch (e) {
-        return false;
-      }
+        var ads = document.createElement("div");
+        ads.innerHTML = "&nbsp;";
+        ads.className = "adsbox";
+        var result = false;
+        try {
+            // body may not exist, that's why we need try/catch
+            document.body.appendChild(ads);
+            result = document.getElementsByClassName("adsbox")[0].offsetHeight === 0;
+            document.body.removeChild(ads);
+        } catch (e) {
+            result = false;
+        }
+        return result;
     },
     getHasLiedLanguages: function(){
       //We check if navigator.language is equal to the first language of navigator.languages
